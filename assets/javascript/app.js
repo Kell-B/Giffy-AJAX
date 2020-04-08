7040aa1f5962f1723da6802ac249accf2ebd0acf
Kell Banks <mckellbanks@gmail.com>
2020-04-08 12:30:43 -0600

    changed theme to memes and removed some unused code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,45 +16,82 @@
 // Rejoice! You just made something really cool.
 
 $(document).ready(function() {
-	var food = [ 'Apples', 'Pizza', 'Steak', 'Pineapple' ];
-
-	var gifBtn;
-
-	gif = $(this).attr('data-name');
-
-	var queryURL =
-		'https://api.giphy.com/v1/gifs/search?q=' + gif + '&api_key=1obEZoPPKMJDJuEVAGU2hkx7hWpjfQBr&limit=10';
-
-	$.ajax({
-		url    : queryURL,
-		method : 'GET'
-	}).then(function(response) {
-		console.log(response);
-	});
+	var memes = [ 'funny memes', 'sad memes', 'weird memes', 'classic memes' ];
 
 	function renderButtons() {
 		$('#buttonView').empty();
-		for (let i = 0; i < food.length; i++) {
-			gifBtn = $('<button>');
-			gifBtn.addClass('btn btn-primary mt-5 mx-3 gifBtn text-capitalize');
-			gifBtn.attr('data-name', food[i]);
-			gifBtn.text(food[i]);
+		for (let i = 0; i < memes.length; i++) {
+			gifBtn = $('<button class="btn btn-primary mt-5 mx-3 gif-btn text-capitalize">');
+			//gifBtn.addClass('btn btn-primary mt-5 mx-3 gif-btn text-capitalize');
+			gifBtn.attr('data-name', memes[i]);
+			gifBtn.text(memes[i]);
 			$('#buttonView').append(gifBtn);
 		}
 	}
 
 	$('#add-btn').on('click', function(event) {
 		event.preventDefault();
-		var newFood = $('#search-bar').val().trim();
+		var newMeme = $('#search-bar').val().trim();
+
 		$('#search-bar').val('');
-		food.push(newFood);
-		console.log(food);
+		memes.push(newMeme);
+		console.log(memes);
 		renderButtons();
 	});
 
-	// var space = ["planets", "stars", "nebulas", "rockets", "telescopes", "galaxy"];
-
 	renderButtons();
-	console.log(renderButtons);
-	console.log(food);
+	// var space = ["planets", "stars", "nebulas", "rockets", "telescopes", "galaxy"];
+	//$(document).on(“click”, “.gif-btn”, displayGifs);
+	$(document).on('click', '.gif-btn', function() {
+		var gifBtn;
+
+		var gif = $(this).attr('data-name');
+
+		var queryURL =
+			'https://api.giphy.com/v1/gifs/search?q=' + gif + '&api_key=ZN5bvG5xzMUp8C2VWT22c1w9BorJLWfP&limit=10';
+
+		console.log(queryURL);
+
+		$.ajax({
+			url    : queryURL,
+			method : 'GET'
+		}).then(function(response) {
+			console.log(response);
+			for (let i = 0; i < response.data.length; i++) {
+				var imageStill = response.data[i].images.downsized_still.url;
+				var imageAnimate = response.data[i].images.downsized_large.url;
+				var rating = response.data[i].rating;
+				var displayRating = $('<p style=‘text-transform: capitalize;’>').html('Gif rating: ' + rating);
+				var newDiv = $('<div>');
+				console.log(rating);
+				var displayGif = $('<img>').attr('src', imageStill);
+				newDiv.append(displayGif);
+				$('#gif-div').prepend(newDiv);
+				$('img').attr('data-state', 'still');
+				$('img').addClass('gif');
+				displayGif.attr('data-animate', imageAnimate);
+				displayGif.attr('data-still', imageStill);
+				newDiv.append(displayRating);
+			}
+		});
+
+		$(document).on('click', '.gif', function() {
+			console.log('hi');
+			var state = $(this).attr('data-state');
+
+			var still = $(this).attr('data-still');
+
+			var animate = $(this).attr('data-animate');
+
+			if (state === 'still') {
+				state = 'animate';
+				$(this).attr('src', animate);
+				$(this).attr('data-state', state);
+			} else if (state === 'animate') {
+				state = 'still';
+				$(this).attr('src', still);
+				$(this).attr('data-state', state);
+			}
+		});
+	});
 });
